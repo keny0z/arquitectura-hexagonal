3d239d21d0eebad6d500542aba4e4fb43a49c30f
keny0z <kevinospinacdn@gmail.com>
2022-05-01 22:06:36 -0500

    estructura base del proyecto

---
+++ USUARIOS_Api/src/test/java/co/com/keny0z/usuarios/USUARIOS_Api/UsuariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.keny0z.usuarios.USUARIOS_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
