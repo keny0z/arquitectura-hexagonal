89e0b702382fe7f80880580406b079034b521fba
keny0z <kevinospinacdn@gmail.com>
2022-05-01 22:40:30 -0500

    agregue validaciones en el constructor de Usuario

--- USUARIOS_Api/src/main/java/co/com/keny0z/usuarios/USUARIOS_Api/dominio/modelo/Usuario.java
+++ USUARIOS_Api/src/main/java/co/com/keny0z/usuarios/USUARIOS_Api/dominio/modelo/Usuario.java
@@ -1,11 +1,20 @@
 package co.com.keny0z.usuarios.USUARIOS_Api.dominio.modelo;
 
+import static co.com.keny0z.usuarios.USUARIOS_Api.dominio.modelo.ValidadorArgumento.validarObligatorio;
+
 public class Usuario {
+
+    private static final String MENSAJE_CLAVE_OBLIGATORIA = "La clave es obligatoria.";
+    private static final String MENSAJE_CORREO_OBLIGATORIO = "El correo electronico es obligatorio.";
 
     private String correoElectronico;
     private String clave;
 
     public Usuario(String correoElectronico, String clave){
+
+        validarObligatorio(correoElectronico, MENSAJE_CORREO_OBLIGATORIO);
+        validarObligatorio(clave, MENSAJE_CLAVE_OBLIGATORIA);
+
         this.correoElectronico = correoElectronico;
         this.clave = clave;
     }
